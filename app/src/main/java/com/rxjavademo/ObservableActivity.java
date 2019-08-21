63d55792ef30cf91732d2c945bc69f90efc9e340
Atul Raj <atul.raj@quytech.com>
2019-08-21 19:09:51 +0530

    Completable/MayBe Operator Added.

--- app/src/main/java/com/rxjavademo/ObservableActivity.java
+++ app/src/main/java/com/rxjavademo/ObservableActivity.java
@@ -5,6 +5,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Completable;
+import io.reactivex.CompletableObserver;
+import io.reactivex.Maybe;
+import io.reactivex.MaybeObserver;
 import io.reactivex.Single;
 import io.reactivex.SingleEmitter;
 import io.reactivex.SingleObserver;
@@ -22,7 +28,13 @@ public class ObservableActivity extends AppCompatActivity {
                     first();
                     break;
                 case R.id.btn_second:
-
+                    second();
+                    break;
+                case R.id.btn_third:
+                    third();
+                    break;
+                case R.id.btn_fourth:
+                    fourth();
                     break;
             }
         }
@@ -34,6 +46,8 @@ public class ObservableActivity extends AppCompatActivity {
         setContentView(R.layout.activity_observable);
         findViewById(R.id.btn_first).setOnClickListener(onClickListener);
         findViewById(R.id.btn_second).setOnClickListener(onClickListener);
+        findViewById(R.id.btn_third).setOnClickListener(onClickListener);
+        findViewById(R.id.btn_fourth).setOnClickListener(onClickListener);
     }
 
     private void first() {
@@ -49,17 +63,17 @@ public class ObservableActivity extends AppCompatActivity {
         SingleObserver<User> singleObserver = new SingleObserver<User>() {
             @Override
             public void onSubscribe(Disposable d) {
-                Log.e(TAG, ">>>>> onSubscribe "+d.isDisposed());
+                Log.e(TAG, ">>>>> onSubscribe " + d.isDisposed());
             }
 
             @Override
             public void onSuccess(User user) {
-                Log.e(TAG,">>>>> onSuccess "+user.toString());
+                Log.e(TAG, ">>>>> onSuccess " + user.toString());
             }
 
             @Override
             public void onError(Throwable e) {
-                Log.e(TAG,">>>>> onError "+e.getMessage());
+                Log.e(TAG, ">>>>> onError " + e.getMessage());
             }
         };
 
@@ -68,7 +82,90 @@ public class ObservableActivity extends AppCompatActivity {
     }
 
     private void second() {
+        Completable completable = Completable.timer(10, TimeUnit.SECONDS);
+
+        CompletableObserver observer = new CompletableObserver() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                Log.e(TAG, ">>>>> onSubscribe ::" + d.isDisposed());
+            }
+
+            @Override
+            public void onComplete() {
+                Log.e(TAG, ">>>>> onComplete");
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.e(TAG, ">>>>> onError");
+            }
+        };
+
+        completable.subscribe(observer);
+    }
+
+    private void third() {
+        Single<String> single = Single.just("Single 1");
+
+        SingleObserver<String> observer = new SingleObserver<String>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                Log.e(TAG, ">>>>> onSubscribe ::" + d.isDisposed());
+            }
+
+            @Override
+            public void onSuccess(String single) {
+                Log.e(TAG, ">>>>> onSuccess ::" + single);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.e(TAG, ">>>>> onError");
+            }
+        };
+
+        single.subscribe(observer);
+    }
+
+    private void fourth() {
+        Maybe<String> maybe = Maybe.just("Hello");
+
+        Maybe<String> empty = Maybe.empty();
+
+        MaybeObserver<String> observer = new MaybeObserver<String>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                Log.e(TAG, ">>>>> onSubscribe ::" + d.isDisposed());
+            }
+
+            @Override
+            public void onSuccess(String s) {
+                Log.e(TAG, ">>>>> onSuccess" + s);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.e(TAG, ">>>>> onError" + e.getMessage());
+            }
+
+            @Override
+            public void onComplete() {
+                Log.e(TAG, ">>>>> onComplete ");
+            }
+        };
+
+//        maybe.subscribe(observer);
+        empty.subscribe(observer);
+    }
+
+    private void fifth() {
+
+    }
+
+    private void sixth() {
 
     }
 
 }
+
+//https://android.jlelse.eu/rxjava-single-maybe-and-completable-8686db42bac8
